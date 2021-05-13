5cc847a4f5815c300b64a703f1e49da9ce846f9d
Jessica Maria Echterhoff <j.m.echt@gmail.com>
2021-05-13 10:53:27 -0700

    refactor ui code and remove unused models

--- streamlit_UI.py
+++ streamlit_UI.py
@@ -25,13 +25,33 @@ def get_next_action(lstm_input, hidden_anchor_state, possible_next_instances_mas
         #Select student to be sampled
         action_idx = select_action(valid_output).item()
     return action_idx
+
+def generate_random_code():
+    letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789'
+    return ''.join(random.choice(letters) for i in range(10))
+
+def display_thank_you(code):
+    st.write("Thanks for contributing to this study.")
+    st.write('Please submit the following code to MTurk:')
+    st.title(code)
+    button_placeholder.empty()
+    placeholder.empty()
+    button_placeholder_2.empty()
+    explanation_placeholder_2.empty()
+    explanation_placeholder.empty()
+
+def save_data(last_decisions, path, code):
+    np.save(f'./data/mturk_review_session_data_{path}_{code}.npy', np.array(last_decisions))
+
         
 def main():            
     action_idx = None
 
     data_paths = ['books_reviews.csv', 'books_reviews_2.csv', 'books_reviews_3.csv', 'books_reviews_4.csv',
      'books_reviews_4.csv', 'books_reviews_5.csv', 'books_reviews_6.csv', 'books_reviews_7.csv']
-    df = pd.read_csv(f"./review_sessions/{data_paths[1]}")
+    random_idx = random.randrange(len(data_paths))
+    path = data_paths[random_idx]
+    df = pd.read_csv(f"./review_sessions/{path}")
 
     possible_next_instances = np.array(df)
     input_size = hidden_size = state_size = 1
@@ -49,10 +69,14 @@ def main():
 
     actor = actor_critic_models.Actor(state_size, review_length).to(device)
     actor.load_state_dict(torch.load('./rl_anchoring/state_dicts/actor_model.pt'))
+
+    #### Init session state to preserve states 
     sess_state = session_state.SessionState(progress=0, action_idx=0, last_decisions=[0], possible_next_instances_mask=possible_next_instances_mask)
-    
     ss = session_state.get(progress=0, action_idx=0, last_decisions=[0], possible_next_instances_mask=possible_next_instances_mask)
+    
     st.title('Book Reviews')
+
+    ####Declare placeholders for visuals 
     explanation_placeholder = st.empty()
     explanation_placeholder.write('Consider the following rating of a book from another user. \
     In total, you will see 50 individual book review texts. Please consider every review individually. \
@@ -65,8 +89,7 @@ def main():
     placeholder.table(df[["summary", "reviewText"]].loc[ss.action_idx])
     button_placeholder = st.empty()
     button_placeholder_2 = st.empty()
-    letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789'
-    
+
     if button_placeholder.button("Yes, I'd like to read the book."):
         ss.last_decisions = ss.last_decisions + [1]
         lstm_input = torch.tensor(np.array(ss.last_decisions, dtype=float)).to(device).to(torch.float)
@@ -78,18 +101,9 @@ def main():
         tmp_mask[action_idx] = False
         ss.possible_next_instances_mask = tmp_mask
         if np.array(ss.possible_next_instances_mask).sum() == 0 :
-            code = ''.join(random.choice(letters) for i in range(10))
-            st.title("Thanks for contributing to this study.")
-            st.write('Please submit the following code to MTurk:')
-            st.title(code)
-            button_placeholder.empty()
-            button_placeholder_2.empty()
-            placeholder.empty()
-            explanation_placeholder_2.empty()
-            explanation_placeholder.empty()
-            np.save(f'mturk_review_data_{code}.npy', np.array(ss.last_decisions))
-
-
+            code = generate_random_code()
+            display_thank_you(code)
+            save_data(ss.last_decisions, path, code)
 
     if button_placeholder_2.button("No, I'd NOT like to read the book."):
         ss.last_decisions = ss.last_decisions + [0]
@@ -101,19 +115,12 @@ def main():
         tmp_mask = ss.possible_next_instances_mask
         tmp_mask[action_idx] = False
         ss.possible_next_instances_mask = tmp_mask
-        if np.array(ss.possible_next_instances_mask).sum() == 0 :
-            st.write("Thanks for contributing to this study.")
-            st.write('Please submit the following code to MTurk:')
-            code = ''.join(random.choice(letters) for i in range(10))
-            st.title(code)
-            button_placeholder.empty()
-            placeholder.empty()
-            button_placeholder_2.empty()
-            explanation_placeholder_2.empty()
-            explanation_placeholder.empty()
-            np.save(f'./data/mturk_review_session_data_{code}.npy', np.array(ss.last_decisions))
-
         
+        if np.array(ss.possible_next_instances_mask).sum() == 0 :
+            code = generate_random_code()
+            display_thank_you(code)
+            save_data(ss.last_decisions, path, code)
+    
 
 if __name__ == "__main__":
     main()
