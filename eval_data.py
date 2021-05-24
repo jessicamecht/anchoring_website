70fb608f91a1fe1ed87399798d7a9bc50c0c6bcd
Jessica Echterhoff <j.m.echt@gmail.com>
2021-05-24 08:26:23 -0700

    add data

---
+++ eval_data.py
@@ -0,0 +1,6 @@
+import numpy as np 
+import pandas as pd 
+
+if __name__ == "__main__":
+    npy_file = pd.read_csv('./data/8/mturk_review_session_data_books_reviews_8.csv_DRGOIQ8BU8.csv')
+    print(npy_file)
